d0e0ac4c335e8b6ffd9e7d2789db6c8166f99274
Angelo Ortiz <aortizy@gmail.com>
2022-11-12 12:50:18 -0300

    add estadoMundial

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -65,4 +65,22 @@ public class RestData {
         
         
     }
+
+        @GetMapping(path = "/estadoMundial", produces = MediaType.APPLICATION_JSON_VALUE)
+        public @ResponseBody Mundial getTotalMundial(){
+
+                LOGGER.log(Level.INFO, "Consulta Mundial");
+
+                RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> call= restTemplate.getForEntity("https://api.covid19api.com/world/total" ,String.class);
+            Mundial response = new Mundial();
+                Gson gson = new Gson();
+        Mundial estado = gson.fromJson(call.getBody().toLowerCase(), Mundial.class);
+        response.setTotalConfirmed(estado.getTotalConfirmed());
+        response.setTotalDeaths(estado.getTotalDeaths());
+        response.setTotalRecovered(estado.getTotalRecovered());
+
+                return response;
+
+        }
 }
